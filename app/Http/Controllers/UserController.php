7599363cbcbc76f86989324d6b1e85788bf84ce5
AdnanRahmat <adnanrahmataonillah@gmail.com>
2023-12-04 14:36:37 +0700

    Submission 3

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+{
+    //parsing data
+    $nama = "Bastian";
+    $titles = ["staff", "kasir"];
+    $usia = 20;
+    $data = [
+        "myName" => $nama,
+        "age" => $usia,
+        "titles" => $titles
+    ];
+    return view('user.index', $data);
+}
+
+public function create()
+{
+    return view('user.create');
+}
+
+public function store(Request $request)
+{
+    // Simpan data user baru
+}
+
+public function show($id)
+{
+    // Tampilkan detail user
+}
+
+public function edit($id)
+{
+    return view('user.edit');
+}
+
+public function update(Request $request, $id)
+{
+    // Perbarui data user
+}
+
+public function destroy($id)
+{
+    // Hapus user
+}
+
+}
